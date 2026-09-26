b356d89836f77c984e69f55c07389ce434cd361a
Abanoub Nashat <nashatabanop@gmail.com>
2026-09-26 17:00:39 +0300

    added a small feature to erase the search bar when pressing Enter with useRef and DOM manipulation.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,33 @@
-import { useState } from "react";
+import { useRef, useEffect } from "react";
 
-export default function SearchBar({query ,setQuery}) {
+export default function SearchBar({ query, setQuery }) {
+  // Right way to select DOM Elements in React to make it declarative.
+  // the .current property is the way we can access that element after referencing it DON'T FORGET IT.
+  const inputElement = useRef(null);
+
+  useEffect(() => {
+    const callback = (e) => {
+      if (document.activeElement === inputElement.current) {
+        return;
+      }
+
+      if (e.code === "Enter") {
+        inputElement.current.focus();
+        setQuery("");
+      }
+    };
+    document.addEventListener("keydown", callback);
+    return () => {
+      document.removeEventListener("keydown", callback);
+    };
+  }, [setQuery]);
+
+  // how to not select DOM Elements:
+  // useEffect(() => {
+  //   const searchBar = document.querySelector('.search');
+  //   searchBar.focus();
+  // }, [])
+
   return (
     <input
       className="search"
@@ -8,6 +35,7 @@ export default function SearchBar({query ,setQuery}) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputElement}
     />
   );
 }
